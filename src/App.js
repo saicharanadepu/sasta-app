6c24629141e1d21f137960ac3337ee18f7991d49
Saicharan.adepu <charan9294@gmail.com>
2023-11-27 13:43:37 -0600

    pushing code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ListElectronicsComponents from './components/ListElectronicsComponents.jsx';
+import AddElectronicsComponent from './components/AddElectronicsComponent.jsx';
+import Headercomponent from './components/Headercomponent.jsx';
+import Footercomponent from './components/Footercomponent.jsx';
+import GroceriesComponents from './components/GroceriesComponent';
+import KitchenComponent from './components/KitchenComponent';
+import GasolineComponent from './components/GasolineComponent';
+import HomeComponent from './components/HomeComponent';
+import AddKitchenItemComponent from './components/AddKitchenItemComponent';
+import AddGroceriesComponent from './components/AddGroceriesComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Headercomponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={HomeComponent}></Route>
+            <Route
+              path="/electronics"
+              component={ListElectronicsComponents}
+            ></Route>
+            <Route
+              path="/AddElectronics"
+              component={AddElectronicsComponent}
+            ></Route>
+            <Route path="/groceries" component={GroceriesComponents}></Route>
+            <Route path="/gasoline" component={GasolineComponent}></Route>
+            <Route path="/kitchenItems" component={KitchenComponent}></Route>
+            <Route
+              path="/AddKitchenItem"
+              component={AddKitchenItemComponent}
+            ></Route>
+            <Route
+              path="/AddGroceries"
+              component={AddGroceriesComponent}
+            ></Route>
+          </Switch>
+        </div>
+        <Footercomponent />
+      </Router>
     </div>
   );
 }
